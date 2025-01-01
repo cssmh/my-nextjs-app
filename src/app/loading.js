a5dfcbcb72e696266b970cd7e34078c908113c51
Momen <touristmomen@gmail.com>
2025-01-01 12:52:57 +0600

    loading, not found

---
+++ src/app/loading.js
@@ -0,0 +1,9 @@
+const loadingPage = () => {
+    return (
+        <div className='text-center my-10'>
+            <span className="loading loading-spinner loading-lg"></span>
+        </div>
+    );
+};
+
+export default loadingPage;
---
+++ src/app/not-found.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div className='text-center my-10'>
+            page not  found
+        </div>
+    );
+};
+
+export default NotFound;
